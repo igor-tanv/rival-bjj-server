8d21fb7e2179cfbb9c813836564f8a2130f9fa3e
Igor Tatarinov <igor.tanv@gmail.com>
2019-12-26 20:00:46 -0500

    change vocabulary

--- tests/contract.test.js
+++ tests/contract.test.js
@@ -1,47 +1,47 @@
 const request = require('supertest')
 const app = require('../src/app')
-const Task = require('../src/models/task')
+const Contract = require('../src/models/contract')
 const {
-    userOneId,
-    userOne,
-    userTwoId,
-    userTwo,
-    taskOne,
-    taskTwo,
-    taskThree,
+    playerOneId,
+    playerOne,
+    playerTwoId,
+    playerTwo,
+    contractOne,
+    contractTwo,
+    contractThree,
     setupDatabase
 } = require('./fixtures/db')
 
 beforeEach(setupDatabase)
 
-test('Should create task for user', async () => {
+test('Should create contract for player', async () => {
     const response = await request(app)
-        .post('/tasks')
-        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .post('/contracts')
+        .set('Authorization', `Bearer ${playerOne.tokens[0].token}`)
         .send({
             description: 'From my test'
         })
         .expect(201)
-    const task = await Task.findById(response.body._id)
-    expect(task).not.toBeNull()
-    expect(task.completed).toEqual(false)
+    const contract = await Contract.findById(response.body._id)
+    expect(contract).not.toBeNull()
+    expect(contract.completed).toEqual(false)
 })
 
-test('Should fetch user tasks', async () => {
+test('Should fetch player contracts', async () => {
     const response = await request(app)
-        .get('/tasks')
-        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .get('/contracts')
+        .set('Authorization', `Bearer ${playerOne.tokens[0].token}`)
         .send()
         .expect(200)
     expect(response.body.length).toEqual(2)
 })
 
-test('Should not delete other users tasks', async () => {
+test('Should not delete other players contracts', async () => {
     const response = await request(app)
-        .delete(`/tasks/${taskOne._id}`)
-        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .delete(`/contracts/${contractOne._id}`)
+        .set('Authorization', `Bearer ${playerTwo.tokens[0].token}`)
         .send()
         .expect(404)
-    const task = await Task.findById(taskOne._id)
-    expect(task).not.toBeNull()
+    const contract = await Contract.findById(contractOne._id)
+    expect(contract).not.toBeNull()
 })
