38bd0ecfc00ce6cb6170810aebef3d7b64916028
Igor Tatarinov <igor.tanv@gmail.com>
2020-03-24 18:15:20 +0700

    updates

--- src/routers/player.js
+++ src/routers/player.js
@@ -17,60 +17,22 @@ const registerPlayer = require('../services/player/registerPlayer')
 let multipart = require('connect-multiparty')
 
 
-router.post('/register', async (req, res) => {
-    console.log('REQ.BODY', req.body)
-    console.log('REQ.FILES', req.files)
-    //need req.body and req.avatar
-    multipart({ uploadDir: path.PUBLIC.AVATAR_PICTURES, maxFieldsSize: 10 * 1024 * 1024 })
-    const player = await registerPlayer.registerPlayer(req.body, req.files)
-    console.log('API', player)
-    if (player.status != 200) {
-        req.flash('error', 'Registration Error')
-        return res.redirect('/register')
-    }
-})
-
-
 // router.post('/register', async (req, res) => {
-//     console.log('BODY',req)
-//     console.log('FILE',req.file)
-//     const buffer = req.file.buffer
-//     const newPlayer = req.body
-
-//     const player = await registerPlayer.registerPlayer(buffer, newPlayer)
-//     console.log('API PLAYER', player)
-
-
-// multerParams(req, res, async function (err) {
-//     if (err) {
-//         if (err.field === 'avatar') {
-//             req.flash('error', 'Avatar size cannot exceed 1MB')
-//             return res.redirect('/register')
-//         } else {
-//             req.flash('error', err.message)
-//             return res.redirect('/register')
-//         }
-//     }
-//     try {
-//         const buffer = await sharp(req.file.buffer).resize({ width: 150, height: 150 }).png().toBuffer()
-//         req.body.avatar = buffer
-//         const player = new Player(req.body)
-//         await player.save()
-//         player.avatar = player.avatar.toString('base64')
-// req.logIn(player, function(err){
-//     if(err) {
+//     console.log('REQ.BODY', req.body)
+//     console.log('REQ.FILES', req.files)
+//     //need req.body and req.avatar
+//     multipart({ uploadDir: path.PUBLIC.AVATAR_PICTURES, maxFieldsSize: 10 * 1024 * 1024 })
+//     const player = await registerPlayer.registerPlayer(req.body, req.files)
+//     console.log('API', player)
+//     if (player.status != 200) {
 //         req.flash('error', 'Registration Error')
 //         return res.redirect('/register')
 //     }
-//     res.render('player-profile.hbs', { player })
 // })
 
-//     } catch (e) {
-//         req.flash('error', 'Something went wrong')
-//         res.redirect('/register')
-//     }
-// })
-//})
+router.post('/register', async (req,res) => {
+    console.log(req.body)
+})
 
 
 router.get('/', async (req, res) => {
