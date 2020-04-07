192994e2fc127d71e526ada557f7ea58443126d4
Igor Tatarinov <igor.tanv@gmail.com>
2020-04-07 20:21:55 +0700

    factored out weight classes and opponents

--- src/helpers/weight.js
+++ src/helpers/weight.js
@@ -1,14 +1,16 @@
 
 const weightLimits = (weightClass) => {
-
   weightClasses = {
-    'Flyweight': { kg: 57.5 },
+    'Flyweight': { kg: '57.5 and under'},
     'Featherweight': { kg: 64 },
     'Lightweight': { kg: 70 },
     'Welterweight': { kg: 76 },
-    'Middleweight': { kg: 88.3 },
+    'Middleweight': { kg: 82.3 },
+    'Cruiserweight': { kg: 88.3 },
     'Light-Heavyweight': { kg: 94.3 },
-    'Heavyweight': { kg: 100.5 }
+    'Heavyweight': { kg: 100.4 },
+    'Super-Heavyweight': { kg: '100.5 and over' },
+    "Absolute": { kg: 'Open Weight'}
   }
 const kilos = weightClasses[weightClass].kg
 const pounds = Math.round(kilos * 2.2)
