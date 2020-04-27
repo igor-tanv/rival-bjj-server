9cc04f876e397739be4e52cad742062dede28cf0
Igor Tatarinov <igor.tanv@gmail.com>
2020-04-28 01:02:19 +0700

    further ES6 updates - untested

--- src/services/contract/getContracts.js
+++ src/services/contract/getContracts.js
@@ -40,53 +40,7 @@ const getContracts = async (id) => {
   return processedContracts
 }
 
-const getContract = async (contractId) => {
-  let contract = await ContractData.getContractByContractId(contractId)
-  if (contract.status === 200) {
-    contract = contract.data
-    let opponent = await PlayerData.getPlayerById(contract.opponentId)
-    let player = await PlayerData.getPlayerById(contract.playerId)
-    date = new Date(contract.datetime * 1000)
-    let months = ["", "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"];
-    let month = months[date.getMonth()]
-    let minutes = date.getMinutes()
-    if (minutes == 0) {
-      minutes = '00'
-    }
-    contract['date'] = {
-      "year": date.getFullYear(),
-      month,
-      "day": date.getDate(),
-      "hour": date.getHours(),
-      minutes
-    }
-
-    contract['kilos'] = weightClass.weightLimits(contract.weightClass)
-    
-    contract['opponent'] = {
-      "avatar": opponent.avatar,
-      "firstName": opponent.firstName,
-      "lastName": opponent.lastName,
-      "gi": opponent.gi,
-      "nogi": opponent.nogi,
-      "school": opponent.school
-    }
-    contract['player'] = {
-      "avatar": player.avatar,
-      "firstName": player.firstName,
-      "lastName": player.lastName,
-      "gi": player.gi,
-      "nogi": player.nogi,
-      "school": player.school
-    }
-    return ({ status: 200, data: contract })
-  }
-  return ({ status: 400, data: contract })
-
-}
-
 module.exports = {
   getContracts,
-  getContract
+
 }
---
+++ src/services/contract/index.js
@@ -0,0 +1,11 @@
+import { getContract } from './getPlayers'
+import { getContracts } from './getPlayer'
+import { registerContract } from './registerPlayer'
+import { updateContract } from './deletePlayerById'
+
+module.exports = {
+  getContract,
+  getContracts,
+  registerContract,
+  updateContract,
+}
