46eed2f86e94ae46449044e51b5f9980a8db1b80
Igor Tatarinov <igor.tanv@gmail.com>
2020-11-26 17:19:38 +0700

    update player service function

--- src/services/player/updatePlayer.js
+++ src/services/player/updatePlayer.js
@@ -1,34 +1,24 @@
-const multipart = require('connect-multiparty')
-const paths = require('../../paths')
-const fs = require('fs')
 const { getPlayer } = require('./getPlayer')
-const PlayersData = require('../../data/PlayerData')
+const PlayersData = require('../../data/PlayerData');
+const PlayerData = require('../../data/PlayerData');
 
-const updatePlayer = async (playerId, updates, avatar) => {
+const updatePlayer = async (playerId, updates) => {
+  try {
+    const allowedUpdates = ['firstName', 'lastName', 'birthYear', 'weightClass', 'school', 'gender', 'avatar']
 
-  const arrFile = avatar.path.split('/')
-  updates.avatar = arrFile[arrFile.length - 1]
-
-
-  // if no avatar undo empty upload created by multipart
-  if (avatar.size == 0) {
-    fs.unlink(paths.PUBLIC.AVATAR_PICTURES + '/' + updates.avatar, function (err) {
-      if (err) throw err
-    })
-    updates.avatar = ''
-  } else if (avatar.size != 0) {
-    const player = await getPlayer(playerId)
-    fs.unlink(paths.PUBLIC.AVATAR_PICTURES + '/' + player.avatar, function (err) {
-      if (err) throw err
-    })
+    const filteredUpdates = Object.keys(updates)
+      .filter(key => allowedUpdates.includes(key))
+      .reduce((newUpdates, key) => {
+        newUpdates[key] = updates[key];
+        return newUpdates;
+      }, {});
+    const player = await PlayerData.updatePlayer(playerId, filteredUpdates)
+    return ({ status: 200, data: player })
   }
-
-  let filteredU = Object.keys(updates).reduce((filter, update) => {
-    if (updates[update]) filter[update] = updates[update];
-    return filter;
-  }, {});
-
-  return await PlayersData.updatePlayer(playerId, filteredU)
+  catch (err) {
+    //await sendAdminEmail(player)
+    return ({ status: 500, data: err })
+  }
 }
 
 module.exports = {
