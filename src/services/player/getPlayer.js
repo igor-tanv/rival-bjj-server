586c2b5d8d447afeb8852bd116b1d230dde890b6
Igor Tatarinov <igor.tanv@gmail.com>
2020-12-02 17:36:55 +0700

    sumRating logic added

--- src/services/player/getPlayer.js
+++ src/services/player/getPlayer.js
@@ -5,12 +5,14 @@ const addContractsRecordQualityratingToPlayer = async (player) => {
   let contracts = await ContractData.getContractsByPlayerOrOpponentId(player.id)
   let sumRating = []
   contracts.forEach((c) => {
+    console.log(c.playerId.toString() === player.id.toString() && c.playerQualityRating != 0, 0)
     if (c.playerId.toString() === player.id.toString() && c.playerQualityRating != 0) sumRating.push(c.playerQualityRating)
     if (c.opponentId.toString() === player.id.toString() && c.playerQualityRating != 0) sumRating.push(c.opponentQualityRating)
   })
+  console.log(contracts, 12)
   player = {
     ...player._doc,
-    qualityRating: Math.round(sumRating.reduce((a, b) => a + b, 0) / sumRating.length),
+    qualityRating: sumRating.length ? Math.round(sumRating.reduce((a, b) => a + b, 0) / sumRating.length) : 0,
     wins: contracts.filter((c) => (c.playerId.toString() === player.id.toString() && c.result === 'win')).length,
     losses: contracts.filter((c) => (c.opponentId.toString() === player.id.toString() && c.result === 'win')).length,
     draws: contracts.filter((c) => c.result === 'draw').length,
