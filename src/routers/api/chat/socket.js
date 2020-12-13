dfe5bd8f29936c242af94c731f80843ca5ccd11b
Minh Phan <minhphan2603@gmail.com>
2020-12-13 13:33:24 +0700

    revert code

--- src/routers/api/chat/socket.js
+++ src/routers/api/chat/socket.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
 
   const wss = new WebSocket.Server({ port: 3002 });
 
-  // wss.on('error', e => console.error(e))
+  wss.on('error', e => console.error(e))
 
   wss.on('connection', function connection(socket) {
     socket.on('message', function incoming(message) {
