1d18aac9b3af59646e5f242c27e170c1ee48c4a0
Minh Phan <minhphan2603@gmail.com>
2020-12-30 21:18:48 +0700

    feat: add Gi/NoGi rank of 2 players into contract

--- src/services/contract/getAllContractsByPlayerId.js
+++ src/services/contract/getAllContractsByPlayerId.js
@@ -1,7 +1,29 @@
 const ContractData = require('../../data/ContractData')
+const PlayerData = require('../../data/PlayerData')
 
 const getAllContractsByPlayerId = async (id) => {
-  return ContractData.getContractsByPlayerOrOpponentId(id)
+  return ContractData.getContractsByPlayerOrOpponentId(id).then(async contracts => {
+    const playerData = await PlayerData.getPlayerById(id);
+    const result = await Promise.all(contracts.map(async contract => {
+      const c = contract.toObject({ getters: true });
+      if (c.playerId.toString() === id) {
+        c.playerGiRank = playerData.gi;
+        c.playerNoGiRank = playerData.nogi;
+        const opponent = await PlayerData.getPlayerById(c.opponentId);
+        c.opponentGiRank = opponent.gi;
+        c.opponentNoGiRank = opponent.nogi;
+      }
+      if (c.opponentId.toString() === id) {
+        c.opponentGiRank = playerData.gi;
+        c.opponentNoGiRank = playerData.nogi;
+        const opponent = await PlayerData.getPlayerById(c.playerId);
+        c.playerGiRank = opponent.gi;
+        c.playerNoGiRank = opponent.nogi;
+      }
+      return c
+    }));
+    return result;
+  })
 }
 
 module.exports = {
