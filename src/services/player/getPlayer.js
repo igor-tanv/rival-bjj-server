6f8ecaed8e4871ce3af8ed46f28f81a8a8c691bd
Igor Tatarinov <igor.tanv@gmail.com>
2021-01-19 11:03:35 +0700

    update record

--- src/services/player/getPlayer.js
+++ src/services/player/getPlayer.js
@@ -1,22 +1,37 @@
 const PlayerData = require('../../data/PlayerData')
 const ContractData = require('../../data/ContractData')
 
-const addContractsRecordQualityratingToPlayer = async (player) => {
-  let contracts = await ContractData.getContractsByPlayerOrOpponentId(player.id)
+
+function getFightRecord(player, contracts) {
+  console.log(6, contracts)
+  return {
+    wins: contracts.filter((c) => (c.playerId.toString() === player.id.toString() && c.result === 'win') || (c.opponentId.toString() === player.id.toString() && c.result === 'loss')).length,
+    losses: contracts.filter((c) => (c.opponentId.toString() === player.id.toString() && c.result === 'win') || (c.playerId.toString() === player.id.toString() && c.result === 'loss')).length,
+    draws: contracts.filter((c) => c.result === 'draw').length,
+  }
+}
+
+function getQualityRating(player, contracts) {
   let sumRating = []
-  contracts.forEach((c) => {
+  contracts.map((c) => {
     if (c.playerId.toString() === player.id.toString() && c.playerQualityRating != 0) sumRating.push(c.playerQualityRating)
     if (c.opponentId.toString() === player.id.toString() && c.playerQualityRating != 0) sumRating.push(c.opponentQualityRating)
+    return
   })
+  return { qualityRating: sumRating.length ? Math.round(sumRating.reduce((a, b) => a + b, 0) / sumRating.length) : 0 }
+}
+
+const addContractsRecordQualityratingToPlayer = async (player) => {
+  let contracts = await ContractData.getContractsByPlayerOrOpponentId(player.id)
+  const { wins, losses, draws } = getFightRecord(player, contracts)
+  const { qualityRating } = getQualityRating(player, contracts)
   player = {
     ...player,
-    qualityRating: sumRating.length ? Math.round(sumRating.reduce((a, b) => a + b, 0) / sumRating.length) : 0,
-    wins: contracts.filter((c) => (c.playerId.toString() === player.id.toString() && c.result === 'win')).length,
-    losses: contracts.filter((c) => (c.opponentId.toString() === player.id.toString() && c.result === 'win')).length,
-    draws: contracts.filter((c) => c.result === 'draw').length,
-    contracts: contracts
-      ? contracts.sort((a, b) => b.datetime - a.datetime)
-      : []
+    qualityRating,
+    wins,
+    losses,
+    draws,
+    contracts: contracts ? contracts.sort((a, b) => b.datetime - a.datetime) : []
   }
   return player
 }
