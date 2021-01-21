e7e493cbb7a45b5570f891c69a4a2b7b6abd06db
Igor Tatarinov <igor.tanv@gmail.com>
2021-01-21 18:17:39 +0700

    elo rating complete

--- src/services/admin/updateContractByContractId.js
+++ src/services/admin/updateContractByContractId.js
@@ -1,17 +1,6 @@
 const ContractData = require('../../data/ContractData')
 const PlayerData = require('../../data/PlayerData')
-const AdminData = require('../../data/AdminData')
-const contractServices = require('../contract/createContract')
-
-const methodValues = {
-  submission: 1,
-  disqualification: 1,
-  forfeit: 1,
-  injury: 1,
-  points: 0.75,
-  draw: 0.5,
-  advantage: 0.25
-}
+const PlayerService = require('../player')
 
 const updateContractByContractId = async (contractId, contractData) => {
   const { winner, method, redRating, blueRating, refereeComments } = contractData
@@ -20,7 +9,6 @@ const updateContractByContractId = async (contractId, contractData) => {
 
   // update contract
   if (winner === 'cancelled') {
-    // update contract status to cancelled and return
     return await ContractData.cancelContract(contractId)
   }
 
@@ -29,18 +17,7 @@ const updateContractByContractId = async (contractId, contractData) => {
   if (winner === 'blue') result = 'loss'
   if (winner === 'draw') result = 'draw'
 
-  const obj = {
-    result,
-    playerQualityRating: parseInt(redRating),
-    opponentQualityRating: parseInt(blueRating),
-    method,
-    refereeComments,
-    completedAt: new Date()
-  }
-
-  console.log(obj, 42)
-
-  const test = await ContractData.updateContract(contractId, {
+  const contract = await ContractData.updateContract(contractId, {
     result,
     playerQualityRating: parseInt(redRating),
     opponentQualityRating: parseInt(blueRating),
@@ -49,13 +26,47 @@ const updateContractByContractId = async (contractId, contractData) => {
     completedAt: new Date()
   })
 
-  console.log(42, test)
-  return test
-
   //update player ranks
-  let red = await PlayerData.getPlayerById(contract.playerId)
-  let blue = await PlayerData.getPlayerById(conctract.opponentId)
-  return null
+  let red = await PlayerService.getPlayer(contract.data.playerId)
+  let blue = await PlayerService.getPlayer(contract.data.opponentId)
+
+  function assignK(player) {
+    const playerRecord = player.wins + player.losses + player.draws
+    if ((playerRecord >= 0 && playerRecord <= 12)) return 80
+    if ((playerRecord >= 13 && playerRecord <= 24)) return 40
+    return 20
+  }
+
+  const matchType = contract.data.type
+  const winProbRed = 1 / (1 + 10 ** ((blue[matchType] - red[matchType]) / 400))
+  const winProbBlue = 1 / (1 + 10 ** ((red[matchType] - blue[matchType]) / 400))
+
+  // refactor 
+  if (winner === 'draw') {
+    newRankRed = red[matchType] + (assignK(red) * (0.5 - winProbRed))
+    newRankBlue = blue[matchType] + (assignK(blue) * (0.5 - winProbBlue))
+  }
+  if (winner === 'red') {
+    newRankRed = red[matchType] + (assignK(red) * (1 - winProbRed))
+    newRankBlue = blue[matchType] + (assignK(blue) * (0 - winProbBlue))
+  }
+  if (winner === 'blue') {
+    newRankRed = red[matchType] + (assignK(red) * (0 - winProbRed))
+    newRankBlue = blue[matchType] + (assignK(blue) * (1 - winProbBlue))
+  }
+
+  newRankRed = Math.round(newRankRed)
+  newRankBlue = Math.round(newRankBlue)
+
+  if (matchType === 'gi') {
+    await PlayerData.updatePlayer(red.id, { gi: newRankRed })
+    await PlayerData.updatePlayer(blue.id, { gi: newRankBlue })
+  }
+  if (matchType === 'nogi') {
+    await PlayerData.updatePlayer(red.id, { nogi: newRankRed })
+    await PlayerData.updatePlayer(blue.id, { nogi: newRankBlue })
+  }
+  return
 }
 
 module.exports = {
