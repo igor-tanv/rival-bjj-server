2badc608d1eaf95e06cff1a9122e85a131baf27b
Igor Tatarinov <igor.tanv@gmail.com>
2021-02-16 07:48:51 +0700

    deleted old ELO

--- src/services/admin/index.js
+++ src/services/admin/index.js
@@ -1,10 +1,8 @@
 
 const getAllContractsByDate = require('../admin/getAllContractsByDate')
-const updatePlayerById = require('./updatePlayerById')
 const updateContractByContractId = require('./updateContractByContractId')
 
 module.exports = {
-  updatePlayerById: updatePlayerById.updatePlayerById,
   getAllContractsByDate: getAllContractsByDate.getAllContractsByDate,
   updateContractByContractId: updateContractByContractId.updateContractByContractId
 }
